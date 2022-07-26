4295cb662c1f3408eff3d93ec80b5fd0d991feb5
phuongnguyen <phuongnguyen@cloud-ace.com>
2022-07-26 08:07:53 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="prj-application"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "prj-application-tfstate"
     prefix = "env/prod"
   }
 }
